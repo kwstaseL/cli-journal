40cb308e786c5471ebf87f32cc658db7915faa44
kwstaseL <kostasel19@gmail.com>
2025-03-06 16:24:09 +0200

    Add Cobra CLI

---
+++ logit/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 Konstantinos Eleftheriou <eleftheriou.konst@gmail.com>
+*/
+package main
+
+import "github.com/kwstaseL/logit/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- pkt/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/kwstaseL/pkt
-
-go 1.24.0
